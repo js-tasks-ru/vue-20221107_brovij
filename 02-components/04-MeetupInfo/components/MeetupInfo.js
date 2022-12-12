3cfc4da4f9735efde63ecf5a1965cf3a49a9d25a
Sheldy <pagedevelopernet@gmail.com>
2022-12-12 21:45:01 +0400

    2-module, 4-task

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -3,19 +3,57 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      defaut: 'Организатор',
+    },
+
+    place: {
+      type: String,
+      default: 'Место',
+    },
+
+    date: {
+      type: Number,
+    },
+  },
+
+  computed: {
+    convertDateTime() {
+      return new Date(this.date).toISOString().split('T')[0]
+    },
+
+    convertDataString() {
+      let options = { day: 'numeric', month: 'long', year: 'numeric', }
+
+      let date = this.convertDate().toLocaleDateString(navigator.language, options)
+
+      return date
+    },
+  },
+
+  methods: {
+    convertDate() {
+      const ms = this.date
+
+      return new Date(ms)
+    }
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="convertDateTime">{{ convertDataString }}</time>
       </li>
     </ul>`,
 });
